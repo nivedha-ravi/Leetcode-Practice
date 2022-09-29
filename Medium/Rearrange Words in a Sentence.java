866f7806680032db6830746b10738d26bc8ee361
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-09-29 12:52:39 +0530

    Create Rearrange Words in a Sentence.java

---
+++ Medium/Rearrange Words in a Sentence.java
@@ -0,0 +1,48 @@
+/*
+Given a sentence text (A sentence is a string of space-separated words) in the following format:
+
+First letter is in upper case.
+Each word in text are separated by a single space.
+Your task is to rearrange the words in text such that all words are rearranged in an increasing order of their lengths. If two words have the same length, arrange them in their original order.
+
+Return the new text following the format shown above.
+
+Example 1:
+
+Input: text = "Leetcode is cool"
+Output: "Is cool leetcode"
+Explanation: There are 3 words, "Leetcode" of length 8, "is" of length 2 and "cool" of length 4.
+Output is ordered by length and the new first word starts with capital letter.
+Example 2:
+
+Input: text = "Keep calm and code on"
+Output: "On and keep calm code"
+Explanation: Output is ordered as follows:
+"On" 2 letters.
+"and" 3 letters.
+"keep" 4 letters in case of tie order by position in original text.
+"calm" 4 letters.
+"code" 4 letters.
+Example 3:
+
+Input: text = "To be or not to be"
+Output: "To be or to be not"
+*/
+class Solution {
+    public String arrangeWords(String text) {
+        String[] t=(text.toLowerCase()).split(" ");
+        Arrays.sort(t,new StringCompare());
+        t[0]=(t[0].substring(0,1)).toUpperCase()+""+t[0].substring(1,t[0].length());
+        return String.join(" ",t);
+    }
+}
+public class StringCompare implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return 1;
+        } else if (s1.length() < s2.length()) {
+            return -1;
+        }
+        return 0;
+    }
+}
