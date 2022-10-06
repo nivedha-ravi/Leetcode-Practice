470eb25bf07ab73d539150b4b73be85cafe16965
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-10-06 19:17:12 +0530

    Create Maximum Product of Three Numbers.java

---
+++ Easy/Maximum Product of Three Numbers.java
@@ -0,0 +1,28 @@
+/*
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+ 
+Constraints:
+
+3 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+*/
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0]*nums[1]*nums[nums.length-1],nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]);
+    }
+}
