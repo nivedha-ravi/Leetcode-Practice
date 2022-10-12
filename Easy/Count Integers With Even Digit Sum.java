8883857dcc0b5b05fa952609a92fc1385f83f874
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-10-12 17:48:17 +0530

    Create Count Integers With Even Digit Sum.java

---
+++ Easy/Count Integers With Even Digit Sum.java
@@ -0,0 +1,39 @@
+/*
+Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
+
+The digit sum of a positive integer is the sum of all its digits.
+
+Example 1:
+
+Input: num = 4
+Output: 2
+Explanation:
+The only integers less than or equal to 4 whose digit sums are even are 2 and 4.    
+Example 2:
+
+Input: num = 30
+Output: 14
+Explanation:
+The 14 integers less than or equal to 30 whose digit sums are even are
+2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28.
+*/
+class Solution {
+    public int countEven(int num) {
+        int count=0;
+        for(int i=1;i<=num;i++)
+        {
+            count+=(sum(i)%2==0)?1:0;
+        }
+        return count;
+    }
+    public int sum(int n)
+    {
+        int sum=0;
+        while(n>0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+}
