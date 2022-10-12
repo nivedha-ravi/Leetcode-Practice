da1392244d227f931db7d608e3cc039a225684a5
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-10-12 18:51:26 +0530

    Create XOR Operation in an Array.java

---
+++ Easy/XOR Operation in an Array.java
@@ -0,0 +1,30 @@
+/*
+You are given an integer n and an integer start.
+
+Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+Return the bitwise XOR of all elements of nums.
+
+Example 1:
+
+Input: n = 5, start = 0
+Output: 8
+Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+Where "^" corresponds to bitwise XOR operator.
+Example 2:
+
+Input: n = 4, start = 3
+Output: 8
+Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+*/
+class Solution {
+    public int xorOperation(int n, int start) {
+        int xor=0;
+        for(int i=0;i<n;i++)
+        {
+            int num=start+(2*i);
+            xor=xor^num;
+        }
+        return xor;
+    }
+}
