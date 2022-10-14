34fbf12333fcdda10202f346d01ab8b2341f4406
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-10-14 20:47:59 +0530

    Create Length of the Longest Alphabetical Continuous Substring.java

---
+++ Medium/Length of the Longest Alphabetical Continuous Substring.java
@@ -0,0 +1,34 @@
+/*
+An alphabetical continuous string is a string consisting of consecutive letters in the alphabet. In other words, it is any substring of the string "abcdefghijklmnopqrstuvwxyz".
+
+For example, "abc" is an alphabetical continuous string, while "acb" and "za" are not.
+Given a string s consisting of lowercase letters only, return the length of the longest alphabetical continuous substring.
+
+Example 1:
+
+Input: s = "abacaba"
+Output: 2
+Explanation: There are 4 distinct continuous substrings: "a", "b", "c" and "ab".
+"ab" is the longest continuous substring.
+Example 2:
+
+Input: s = "abcde"
+Output: 5
+Explanation: "abcde" is the longest continuous substring.
+*/
+class Solution {
+    public int longestContinuousSubstring(String s) {
+        int max=0;
+        String alpha="abcdefghijklmnopqrstuvwxyz";
+        for(int i=0;i<alpha.length();i++)
+        {
+          for(int j=i;j<alpha.length();j++)
+          {
+            String str=alpha.substring(i,j+1);
+            max=(s.contains(str) && str.length()>max)?str.length():max;
+          }
+        }
+      return max;
+    }
+}
+ 
