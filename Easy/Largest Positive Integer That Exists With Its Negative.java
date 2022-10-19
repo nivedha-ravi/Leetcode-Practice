9581df74ea4b074456535d5384a591116945e92d
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-10-19 19:54:57 +0530

    Create Largest Positive Integer That Exists With Its Negative.java

---
+++ Easy/Largest Positive Integer That Exists With Its Negative.java
@@ -0,0 +1,36 @@
+/*
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+
+Example 1:
+
+Input: nums = [-1,2,-3,3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.
+Example 2:
+
+Input: nums = [-1,10,6,7,-7,1]
+Output: 7
+Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+Example 3:
+
+Input: nums = [-10,8,6,7,-2,-3]
+Output: -1
+Explanation: There is no a single valid k, we return -1.
+*/
+class Solution {
+    public int findMaxK(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        int large=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+          set.add(nums[i]);
+          if(set.contains(nums[i]*-1))
+          {
+            large=Math.max((nums[i]<0)?nums[i]*-1:nums[i],large);
+          }
+        }
+        return large;
+    }
+}
