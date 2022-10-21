283b5de039bb0024017145f0ff4e48ab47aa7906
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2022-10-21 20:32:35 +0530

    Create Maximum 69 Number.java

---
+++ Easy/Maximum 69 Number.java
@@ -0,0 +1,41 @@
+/*
+You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+Example 1:
+
+Input: num = 9669
+Output: 9969
+Explanation: 
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666.
+The maximum number is 9969.
+Example 2:
+
+Input: num = 9996
+Output: 9999
+Explanation: Changing the last digit 6 to 9 results in the maximum number.
+Example 3:
+
+Input: num = 9999
+Output: 9999
+Explanation: It is better not to apply any change.
+*/
+class Solution {
+    public int maximum69Number (int num) {
+        char[] s=Integer.toString(num).toCharArray();
+        int max=num;
+        for(int i=s.length-1;i>=0;i--)
+        {
+          s[i]=(s[i]=='6')?'9':'6';
+          max=Math.max(Integer.parseInt(new String(s)),max);
+          s[i]=(s[i]=='6')?'9':'6';
+        }
+      return max;
+        
+        
+    }
+}
