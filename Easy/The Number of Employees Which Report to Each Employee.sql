53ad030c2b7023e576b87e0c1a694faa5d1e57aa
Nivedha Ravi <63157339+nivedha-ravi@users.noreply.github.com>
2023-06-09 21:10:57 +0530

    Create The Number of Employees Which Report to Each Employee.sql

---
+++ Easy/The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,44 @@
+Table: Employees
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+employee_id is the primary key for this table.
+This table contains information about the employees and the id of the manager they report to. Some employees do not report to anyone (reports_to is null). 
+ 
+For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+
+Write an SQL query to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+
+Return the result table ordered by employee_id.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+---------+------------+-----+
+| employee_id | name    | reports_to | age |
++-------------+---------+------------+-----+
+| 9           | Hercy   | null       | 43  |
+| 6           | Alice   | 9          | 41  |
+| 4           | Bob     | 9          | 36  |
+| 2           | Winston | null       | 37  |
++-------------+---------+------------+-----+
+Output: 
++-------------+-------+---------------+-------------+
+| employee_id | name  | reports_count | average_age |
++-------------+-------+---------------+-------------+
+| 9           | Hercy | 2             | 39          |
++-------------+-------+---------------+-------------+
+Explanation: Hercy has 2 people report directly to him, Alice and Bob. Their average age is (41+36)/2 = 38.5, which is 39 after rounding it to the nearest integer.
+
+# Write your MySQL query statement below
+
+select e.employee_id,e.name,count(e1.reports_to) as reports_count,round(sum(e1.age)/count(e1.reports_to)) as average_age from employees e inner join employees e1 on e.employee_id=e1.reports_to group by e1.reports_to having count(e1.reports_to)>=1 order by e.employee_id
